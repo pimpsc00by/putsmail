7cb798772ba66dc3ebc6a7fa0f840b32b7ba75ea
Pablo <pablohstc@gmail.com>
2011-01-20 20:19:18 -0200

    Addding func to remember to and token

--- app/controllers/puts_controller.rb
+++ app/controllers/puts_controller.rb
@@ -1,9 +1,13 @@
 class PutsController < ApplicationController
   
   def index
+    @to = session[:to]
+    @token = session[:token]
   end
   
   def puts_mail
+    session[:to] = params[:mail]
+    session[:token] = params[:token]
     puts_mail = PutsMail.new
     puts_mail.puts_mail(params[:mail], params[:token], params[:subject], params[:body])
     render :text => puts_mail.errors.to_json
