3e7103a7b4a176e47d3289ed18d08bb9369ca29b
Pablo <pablohstc@gmail.com>
2011-01-22 00:18:39 -0200

    rollback previous temporaly change

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,7 @@ class UserMailer < ActionMailer::Base
   
   def registration_confirmation(user)  
     @user = user
-    # mail(:to => user.mail, :subject => "Puts Mail - Token")  
-    mail(:to => user.mail, :subject => "Puts Mail - Congrats! You received a new friendly token")  
+    mail(:to => user.mail, :subject => "Puts Mail - Token")  
   end
   
 end
