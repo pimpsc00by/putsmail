40d5a6417b39c3ccd972777e5915e757ccb4324c
Pablo <pablohstc@gmail.com>
2011-01-22 00:12:19 -0200

    Temporaly change

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
   def registration_confirmation(user)  
     @user = user
     # mail(:to => user.mail, :subject => "Puts Mail - Token")  
-    mail(:to => user.mail, :subject => "OMG! Your token was replaced by a friendly token")  
+    mail(:to => user.mail, :subject => "Puts Mail - Congrats! You received a new friendly token")  
   end
   
 end
