14c1507885d9814e644610e10ccb17ffc1fd09cc
Pablo Cantero <pablo@pablocantero.com>
2012-03-23 18:08:59 -0300

    Fix auto mailer test

--- spec/mailers/test_mail_mailer_spec.rb
+++ spec/mailers/test_mail_mailer_spec.rb
@@ -2,12 +2,14 @@ require "spec_helper"
 
 describe TestMailMailer do
   describe "test_mail" do
-    let(:mail) { TestMailMailer.test_mail }
+    let(:mail) { 
+      TestMailMailer.test_mail TestMail.create(:body => "Hi", subject: "Test mail", recipients: ["pablo@pablocantero.com"])
+    }
 
     it "renders the headers" do
       mail.subject.should eq("Test mail")
-      mail.to.should eq(["to@example.org"])
-      mail.from.should eq(["from@example.com"])
+      mail.to.should eq(["pablo@pablocantero.com"])
+      mail.from.should eq(["test@putsmail.com"])
     end
 
     it "renders the body" do
