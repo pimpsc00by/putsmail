5d28d51f1078b253f5099b84f644248d67fc19ff
Pablo Cantero <pablo@pablocantero.com>
2012-03-26 11:32:32 -0300

    Adding try v2 link

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120131190946) do
+ActiveRecord::Schema.define(:version => 20120326013814) do
 
   create_table "properties", :force => true do |t|
     t.string   "key"
@@ -18,6 +19,24 @@ ActiveRecord::Schema.define(:version => 20120131190946) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "test_mail_users", :force => true do |t|
+    t.integer  "test_mail_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "test_mails", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "subject"
+    t.text     "body"
+    t.string   "token"
+    t.integer  "sent_count", :default => 0
+  end
+
+  add_index "test_mails", ["token"], :name => "index_test_mails_on_token", :unique => true
 
   create_table "users", :force => true do |t|
     t.string   "mail",       :null => false
