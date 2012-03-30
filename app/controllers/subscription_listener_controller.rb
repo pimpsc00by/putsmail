730b70a1bd76288e504db2b4f5ce217e0fcda8fd
Pablo Cantero <pablo@pablocantero.com>
2012-03-29 23:12:13 -0300

    Test 2

--- app/controllers/subscription_listener_controller.rb
+++ app/controllers/subscription_listener_controller.rb
@@ -2,13 +2,13 @@ class SubscriptionListenerController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
   def subscribe
-    Rails.logger.info "subscribe/message.x_from_header: #{params[:x_from_header]}" #print the decoded body to the logs
+    Rails.logger.error "subscribe/message.x_from_header: #{params[:x_from_header]}" #print the decoded body to the logs
     # Do some other stuff with the mail message
     render :text => 'success', :status => 200 # a status of 404 would reject the mail
   end
   
   def unsubscribe
-    Rails.logger.info "unsubscribe/message.x_from_header: #{params[:x_from_header]}" #print the decoded body to the logs
+    Rails.logger.error "unsubscribe/message.x_from_header: #{params[:x_from_header]}" #print the decoded body to the logs
     # Do some other stuff with the mail message
     render :text => 'success', :status => 200 # a status of 404 would reject the mail
   end
