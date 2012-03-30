5a3436d73aad9db15e2c9024d1d76fc6e1992920
Pablo Cantero <pablo@pablocantero.com>
2012-03-30 00:13:09 -0300

    cleanup from

--- app/controllers/subscription_listener_controller.rb
+++ app/controllers/subscription_listener_controller.rb
@@ -1,22 +1,28 @@
 class SubscriptionListenerController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
+  before_filter :x_from_header_to_user
+  
   def subscribe
-    from = params[:x_from_header].to_a.first
-    if user = User.find_by_mail(from)
-      user.subscribed = true
-      user.save
+    if @user
+      @user.subscribed = true
+      @user.save
     end
     render :text => 'success', :status => 200
   end
   
   def unsubscribe
-    from = params[:x_from_header].to_a.first
-    if user = User.find_by_mail(from)
-      user.subscribed = false
-      user.save
+    if @user
+      @user.subscribed = false
+      @user.save
     end
     render :text => 'success', :status => 200 # a status of 404 would reject the mail
+  end
+  
+  private
+  def x_from_header_to_user
+    from = params[:x_from_header].to_s.gsub /\[|\]|\"|\s/, ""
+    @user = User.find_by_mail(from)
   end
 end
 
