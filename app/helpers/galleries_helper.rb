03811e14db27fbe1dc6e3a448ccd5cd5e6afd0c4
Pablo Cantero <pablo@pablocantero.com>
2012-06-07 18:53:22 -0300

    fixing security hash

--- app/helpers/galleries_helper.rb
+++ app/helpers/galleries_helper.rb
@@ -2,12 +2,20 @@ require 'digest/md5'
   
 module GalleriesHelper
   def url2png_image_tag url
-    security_hash = Digest::MD5.hexdigest(Url2png.private_key + url)
-    image_tag "http://api.url2png.com/v3/#{Url2png.private_key}/#{security_hash}/400x300/#{url}"
+    image_tag "http://api.url2png.com/v3/#{Url2png.api_key}/#{security_hash(url)}/400x300/#{url}"
   end
   
   def url2png_image_url url
-    security_hash = Digest::MD5.hexdigest(Url2png.private_key + url)
-    "http://api.url2png.com/v3/#{Url2png.private_key}/#{security_hash}/FULL/#{url}"
+    "http://api.url2png.com/v3/#{Url2png.api_key}/#{security_hash(url)}/FULL/#{url}"
+  end
+  
+  private
+  
+  def security_hash url
+    Digest::MD5.hexdigest("#{Url2png.private_key}+#{url}")
   end
 end
+# boa
+# http://api.url2png.com/v3/P4FD0EFDE615DF/e76a75015b1a6e3cfffb6ef02c9281f2/300x300/http://putsmail.com/galleries/3810
+# ruim
+# http://api.url2png.com/v3/SE2C794374A4CE/5487be0637243806774bda7001861b8c/400x300/http://putsmail.com/galleries/3810
