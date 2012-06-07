2f9c47bb542a7842136c3deac2cc785b51300753
Pablo Cantero <pablo@pablocantero.com>
2012-06-07 18:41:26 -0300

    url2png definitely doesn't work, doing it by hand

--- app/helpers/galleries_helper.rb
+++ app/helpers/galleries_helper.rb
@@ -1,2 +1,13 @@
+require 'digest/md5'
+  
 module GalleriesHelper
+  def url2png_image_tag url
+    security_hash = Digest::MD5.hexdigest(Url2png.private_key + url)
+    image_tag "http://api.url2png.com/v3/#{Url2png.private_key}/#{security_hash}/400x300/#{url}"
+  end
+  
+  def url2png_image_url url
+    security_hash = Digest::MD5.hexdigest(Url2png.private_key + url)
+    "http://api.url2png.com/v3/#{Url2png.private_key}/#{security_hash}/FULL/#{url}"
+  end
 end
--- config/initializers/url2png.rb
+++ config/initializers/url2png.rb
@@ -1,4 +1,5 @@
 Url2png.config({
     api_key: 'P4FD0EFDE615DF',
-    private_key: 'SE2C794374A4CE'
+    private_key: 'SE2C794374A4CE',
+    api_version: 'v4'
 })
