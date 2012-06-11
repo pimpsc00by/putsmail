3c46b44e51a31e23f18a585e995bc7787d10e74b
Pablo Cantero <pablo@pablocantero.com>
2012-06-10 23:54:45 -0300

    Fixing the tests

--- spec/controllers/test_mails_controller_spec.rb
+++ spec/controllers/test_mails_controller_spec.rb
@@ -4,7 +4,8 @@ describe TestMailsController do
 
   describe "GET 'show'" do
     it "returns http success" do
-      get 'show'
+      test_mail = Factory :test_mail
+      get 'show', id: test_mail.id
       response.should be_success
     end
   end
