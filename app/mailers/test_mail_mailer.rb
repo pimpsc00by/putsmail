c0000020714b87ec8c725ed9634f9f579800cc4d
Pablo Cantero <pablo@pablocantero.com>
2012-06-18 12:12:19 -0300

    Testing recipients

--- app/mailers/test_mail_mailer.rb
+++ app/mailers/test_mail_mailer.rb
@@ -4,11 +4,7 @@ class TestMailMailer < ActionMailer::Base
   def test_mail mail
     @mail_body = mail.body
     @mail_token = mail.token
-    to = mail.active_users.first.mail
-    if mail.active_users.size > 1
-      cc = mail.active_users.slice(1, mail.active_users.size)
-      cc.map! { | u | u.mail}
-    end
-    mail(to: to, cc: cc, subject: mail.subject, reply_to: to)
+    to = mail.active_users.collect &:mail
+    mail(to: to, subject: mail.subject, reply_to: to)
   end
 end
--- spec/mailers/test_mail_mailer_spec.rb
+++ spec/mailers/test_mail_mailer_spec.rb
@@ -1,14 +1,14 @@
 require "spec_helper"
 
 describe TestMailMailer do
-  describe "test_mail" do
-    let(:mail) { 
+  describe "Send email" do
+    let(:mail) do
       @mail = Factory :test_mail, subject: "Test mail", body: "Hi"
       @mail.test_mail_users.build user: Factory(:user, mail: "pablo@pablocantero.com")
       @mail.save
       TestMailMailer.test_mail @mail
-    }
-
+    end
+    
     it "renders the headers" do
       mail.subject.should eq @mail.subject
       mail.to.should eq [@mail.users.first.mail]
@@ -21,6 +21,20 @@ describe TestMailMailer do
     
     it "renders the token" do
       mail.body.encoded.should match(@mail.token)
+    end
+  end  
+  
+  describe "Recipients" do
+    let(:three_mail) do
+      @mail = Factory :test_mail, subject: "Test mail", body: "Hi"
+      @mail.test_mail_users.build user: Factory(:user)
+      @mail.test_mail_users.build user: Factory(:user)
+      @mail.test_mail_users.build user: Factory(:user)
+      @mail.save
+      TestMailMailer.test_mail @mail
+    end
+    it "should add more recipients" do
+      three_mail.to.should eq @mail.active_users.collect &:mail
     end
   end
 end
