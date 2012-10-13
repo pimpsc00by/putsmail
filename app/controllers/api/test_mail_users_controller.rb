2fac8fcd35b97207ff04241535802956070f9ed2
Pablo Cantero <pablo@pablocantero.com>
2012-10-13 17:46:33 -0300

    Removes whitespaces

--- app/controllers/api/test_mail_users_controller.rb
+++ app/controllers/api/test_mail_users_controller.rb
@@ -8,25 +8,25 @@ class Api::TestMailUsersController < ApplicationController
     test_mail_user = @test_mail.test_mail_users.create user: user
     respond_with test_mail_user, location: api_test_mail_user_url(test_mail_user)
   end
-  
-  def show    
+
+  def show
     test_mail_user = @test_mail.test_mail_users.find params[:id]
     respond_with test_mail_user
   end
-  
+
   def index
     respond_with @test_mail.test_mail_users.all
   end
-  
+
   def update
     test_mail_user = @test_mail.test_mail_users.find params[:id]
     respond_with test_mail_user.update_attributes(params[:test_mail_user])
   end
-  
+
   def destroy
-    respond_with @test_mail.test_mail_users.destroy(params[:id])    
+    respond_with @test_mail.test_mail_users.destroy(params[:id])
   end
-  
+
   private
   def load_test_mail
     @test_mail = TestMail.find_by_token cookies[:last_test_mail_id]
